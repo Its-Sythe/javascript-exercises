83229958b0c1acdfb85516ad8f9967ebcc749dc2
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-05 19:36:19 +0300

    Completed the exercise
    Wow that was simpler than I anticipated, yet again another for loop to loop through the numbers between the start number to the end number and incrementing a sum variable.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let sum = 0
+    for (let num = startNum; num <= endNum; num++) {
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line
