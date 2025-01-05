9b7b950f82525c590287979fb2d94fc62d1a9b01
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-05 19:56:25 +0300

    Finalized the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 const convertToCelsius = function(temperature) {
-  return ((temperature - 32) * 5 / 9);
+  return Math.round(((temperature - 32) * (5 / 9) * 10) / 10);
 };
 
 const convertToFahrenheit = function(temperature) {
-  return (temperature * ((9 / 5) + 32))
+  return Math.round((temperature * ((9 / 5) + 32) * 10) / 10);
 };
+
+console.log(convertToCelsius(231.2));
 
 // Do not edit below this line
 module.exports = {
