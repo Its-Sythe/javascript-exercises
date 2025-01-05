fcfb2a163f23654c145e27d8c11fb55c7ac1356b
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-05 19:28:48 +0300

    Completed the exercise
    Again with a drastically different solutin from the given solutions but still a for loop came in clutch and saved the day, getting the index of the elements to be removed and a splice made the solution functional

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    for (let x = 0; x <= elementsToRemove.length - 1; x++) {
+        const arrayIndex = array.indexOf(elementsToRemove[x]);
+        array.splice(arrayIndex, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
