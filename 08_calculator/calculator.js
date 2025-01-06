41985c5ee09bcc12ecded06d000a362dad1f9ef7
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-06 06:39:04 +0300

    Completed The Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  let sum = 0;
+  for (let i = 0; i <= numbers.length - 1; i++) {
+    sum += numbers[i];
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+	let sum = 0;
+  for (let i = 0; i <= numbers.length - 1; i++) {
+    sum += numbers[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  let product = 1;
+  for (let i = 0; i <= numbers.length - 1; i++) {
+    product *= numbers[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	return num ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (num; num > 0; num--) {
+    total *= num;
+  }
+  return total;
 };
+
+console.log(power(2, 3));
 
 // Do not edit below this line
 module.exports = {
