41826e495e0e145f223fac95beaf7d46a766638f
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-06 10:56:25 +0300

    Completed using the simpler method

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let sequence = [0, 1];
+    for (let i = 2; i <= number; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence[number];
 };
 
 // Do not edit below this line
