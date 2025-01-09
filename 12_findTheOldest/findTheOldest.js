fb1588c42619750a602104361dd020f8a199b84b
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-09 17:45:03 +0300

    Completed the exercise
    Got confused by the test didnt know I had to return the object of the oldest individul, was a good time, my solution probably is wackier than necessary.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,29 @@
 const findTheOldest = function(people) {
-    let age;
+    let oldestObject = [];
+    let nameOfOldest = "";
+    let age = [];
     const date = new Date();
     const currentYear = date.getFullYear();
     let oldestAge = 0;
     for (let i = 0; i <= people.length - 1; i++) {
         if (people[i].yearOfDeath) {
-            age = people[i].yearOfDeath - people[i].yearOfBirth;
+            age.push(people[i].yearOfDeath - people[i].yearOfBirth);
         } else {
-            age = currentYear - people[i].yearOfBirth;
+            age.push(currentYear - people[i].yearOfBirth);
         }
-       if (age > oldestAge) {
-        oldestAge = age;
-       }
     }
-    return oldestAge;
+    for (let x in age) {
+        if (age[x] > oldestAge) {
+            oldestAge = age[x];
+            nameOfOldest = people[x].name;
+        }
+    }
+    for (let j = 0; j <= people.length - 1; j++) {
+        if (people[j].name == nameOfOldest) {
+            oldestObject = people[j];
+        }
+    }
+    return oldestObject;
 };
 
 // Do not edit below this line
