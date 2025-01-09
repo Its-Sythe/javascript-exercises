12e0190e93a5212cb9add11aa9d973a40ffd40aa
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-09 06:43:18 +0300

    Completed the exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  }
+]
+const getTheTitles = function(book) {
+  const bookTitle = []
+  for (let i = 0; i <= book.length - 1; i++) {
+    let foundTitle = book[i].title;
+    bookTitle.push(foundTitle);
+  }
+  return bookTitle;
 };
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
