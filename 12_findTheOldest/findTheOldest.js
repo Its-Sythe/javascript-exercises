2a23abd65a0b2ef93bc31475846309abe06f272f
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-09 15:00:18 +0300

    Slightly completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let age;
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    let oldestAge = 0;
+    for (let i = 0; i <= people.length - 1; i++) {
+        if (people[i].yearOfDeath) {
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else {
+            age = currentYear - people[i].yearOfBirth;
+        }
+       if (age > oldestAge) {
+        oldestAge = age;
+       }
+    }
+    return oldestAge;
 };
 
 // Do not edit below this line
